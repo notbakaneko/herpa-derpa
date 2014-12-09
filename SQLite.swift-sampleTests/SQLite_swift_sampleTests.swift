c8cd776251ffd38c84f28a956708d1c46b36fa1c
bakaneko <bakaneko@pixiv.com>
2014-12-09 17:25:24 +0900

    Initial Commit

---
+++ SQLite.swift-sampleTests/SQLite_swift_sampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  SQLite_swift_sampleTests.swift
+//  SQLite.swift-sampleTests
+//
+//  Created by bakaneko on 9/12/2014.
+//  Copyright (c) 2014 nekonyan. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SQLite_swift_sampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
