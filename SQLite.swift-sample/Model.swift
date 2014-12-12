9f18b1200b71e2a5341b5982c4e89d5d462be3a5
bakaneko <bakaneko@pixiv.com>
2014-12-12 18:41:33 +0900

    try to move basic patterns into a base class

--- SQLite.swift-sample/Model.swift
+++ SQLite.swift-sample/Model.swift
@@ -14,6 +14,10 @@ let db = Database()
 
 protocol ModelType {
     init(row: Row)
+
+    func save()
+    func delete(#cascade: Bool) -> Int?
+    func delete() -> Int?
 }
 
 protocol GenericModelType: ModelType {
@@ -22,15 +26,71 @@ protocol GenericModelType: ModelType {
 
 class Base: GenericModelType {
     typealias T = Base
+    typealias PrimaryKeyType = Int
+
+    var primaryKeyValue: PrimaryKeyType {
+        assert(1 == 0, "Must override")
+        return 0
+    }
+
     init () {}
 
     required init(row: Row) {
 
     }
+
+    func save() {
+        if primaryKeyValue > 0 {
+            _update()
+        } else {
+            _insert()
+        }
+
+        _cascadingUpdate()
+    }
+
+    func delete(#cascade: Bool) -> Int? {
+        if cascade {
+            _cascadingDelete()
+        }
+
+        return _delete()
+    }
+
+    func delete() -> Int? {
+        return delete(cascade: true)
+    }
+
+
+    // MARK:- implementation
+    func _update() {
+        assert(1 == 0, "Must override")
+    }
+
+    func _insert() {
+        assert(1 == 0, "Must override")
+    }
+
+    func _cascadingUpdate() {
+//        assert(1 == 0, "Must override")
+    }
+
+    func _delete() -> Int? {
+        assert(1 == 0, "Must override")
+    }
+
+    func _cascadingDelete() -> Int? {
+//        assert(1 == 0, "Must override")
+        return 0
+    }
 }
 
 class Model: Base {
     typealias T = Model
+    typealias PrimaryKeyType = Int
+    override var primaryKeyValue: PrimaryKeyType {
+        return id
+    }
 
     var id = 0
     var name: String
@@ -82,51 +142,59 @@ class Model: Base {
     }
 
 
-    func save() {
-        if id > 0 {
-            if let result = Storage.models.filter(Storage.id == id).update(
-                Storage.name <- name,
-                Storage.unique <- unique,
-                Storage.optional <- optional) {
-                    debugPrintln(result)
-            }
-        } else {
-            if let insertedId = Storage.models.insert(
-                Storage.name <- name,
-                Storage.unique <- unique,
-                Storage.optional <- optional) {
-                    debugPrintln(insertedId)
-                    id = insertedId
-            } else {
-                debugPrint("Cannot insert Model with id: \(id)")
-            }
+    override func _update() {
+        if let result = Storage.models.filter(Storage.id == id).update(
+            Storage.name <- name,
+            Storage.unique <- unique,
+            Storage.optional <- optional) {
+                debugPrintln(result)
         }
+    }
 
+    override func _insert() {
+        if let insertedId = Storage.models.insert(
+            Storage.name <- name,
+            Storage.unique <- unique,
+            Storage.optional <- optional) {
+                debugPrintln(insertedId)
+                id = insertedId
+        } else {
+            debugPrint("Cannot insert Model with id: \(id)")
+        }
+    }
+
+    override func _cascadingUpdate() {
         if let details = _details {
             for detail in details {
                 detail.modelId = id
                 detail.save()
             }
         }
-
     }
 
-    func delete(cascade: Bool = true) -> Int? {
-        if cascade {
-            if let count = DetailModel.Storage.models.filter(DetailModel.Storage.modelId == id).delete()? {
-                debugPrintln("id: \(id), \(count) nested rows deleted")
-            }
-        }
-
+    override func _delete() -> Int? {
         let count = Storage.models.filter(Storage.id == id).delete()?
         debugPrintln("id: \(id), \(count) rows deleted")
         return count
+    }
+
+    override func _cascadingDelete() -> Int? {
+        if let count = DetailModel.Storage.models.filter(DetailModel.Storage.modelId == id).delete()? {
+            debugPrintln("id: \(id), \(count) nested rows deleted")
+            return count
+        }
+
+        return nil
     }
 }
 
 
 class DetailModel: Base {
     typealias T = DetailModel
+    typealias PrimaryKeyType = Int
+    override var primaryKeyValue: PrimaryKeyType {
+        return id
+    }
 
     var id = 0
     var modelId = 0
@@ -156,7 +224,7 @@ class DetailModel: Base {
         }
     }
 
-    func save() {
+    override func _insert() {
         if let insertedId = Storage.models.insert(
             Storage.modelId <- modelId) {
                 debugPrintln(insertedId)
@@ -164,7 +232,14 @@ class DetailModel: Base {
         }
     }
 
-    func delete(cascade: Bool = true) -> Int? {
+    override func _update() {
+        if let result = Storage.models.filter(Storage.id == id).update(
+            Storage.modelId <- modelId) {
+                debugPrintln(result)
+        }
+    }
+
+    override func _delete() -> Int? {
         let count = Storage.models.filter(Storage.id == id).delete()?
         debugPrintln("id: \(id), \(count) rows deleted")
         return count
